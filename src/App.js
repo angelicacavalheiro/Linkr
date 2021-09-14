e1c98f93099e9e6d153090e09a1f66e0465b5215
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-09-14 14:03:15 -0300

    routes and library added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState, useContext } from 'react';
+
 export default function App() {
     return(
-        <h1>Hello Word</h1>
+        <BrowserRouter>
+        <Switch>
+
+        <UserContext.Provider >
+
+          <Route path="/sign-up" exact>
+            <Sign-up />
+          </ Route> 
+
+          <Route path="/" exact>
+            <Login />
+          </ Route> 
+
+          <Route path="/timeline" exact>
+            <Timeline />
+          </ Route>  
+
+          <Route path="/my-posts" exact>
+            <MyPosts />
+          </ Route>
+
+          <Route path="/user/:id" exact>
+            <User />
+          </ Route>
+
+          <Route path="/hashtag/:hashtag" exact>
+            <Hashtag />
+          </ Route>
+
+          <Route path="/my-likes" exact>
+            <Hashtag />
+          </ Route>
+        
+          </UserContext.Provider>
+
+      </ Switch>       
+    </ BrowserRouter>   
     )
 }
