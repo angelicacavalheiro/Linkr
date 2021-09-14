9c52d4a43f34cf21dd69e6c0335108339ae7fc18
liggia <liggia.cruz@gmail.com>
2021-09-14 19:39:58 -0300

    testing

--- src/components/MyPosts/MyPostsPage.js
+++ src/components/MyPosts/MyPostsPage.js
@@ -5,7 +5,7 @@ export default function MyPostPage(){
     return(
         <Page>
              <Title>
-            <h2>my posts</h2>
+            <h2>my posts oiiiii</h2>
             </Title>
         </Page>
        
