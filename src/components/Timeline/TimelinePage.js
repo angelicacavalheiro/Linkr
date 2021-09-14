09a471d5808c3a90a455482f23c979e63b7bed3d
Diego Mamede Nogueira <diegomamede@poli.ufrj.br>
2021-09-14 16:38:37 -0300

    Titulo da pagina feito

--- src/components/Timeline/TimelinePage.js
+++ src/components/Timeline/TimelinePage.js
@@ -1,5 +1,18 @@
+import { ContainerBoxClass, ContainerCenterClass, ColunaPostsClass, PageTitleClass } from "../../sharedStyles/sharedStyles"
 export default function TimelinePage () {
     return(
-        <h1>Timeline</h1>
+        <>
+        <ContainerBoxClass>
+            <ContainerCenterClass>
+                <ColunaPostsClass>
+                    <PageTitleClass>TimeLine</PageTitleClass>
+                    <h1>Timeline</h1>
+                </ColunaPostsClass>
+                
+                <h1>Trending</h1>
+            </ContainerCenterClass>
+        </ContainerBoxClass>
+        
+        </>
     )
 }
