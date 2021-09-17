2e6eea4ad1f7eaf89facdba3636f91c575460b9f
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-09-17 16:09:39 -0300

    change color added

--- src/sharedComponents/Likes.js
+++ src/sharedComponents/Likes.js
@@ -1,43 +1,32 @@
 import { useContext, useState } from "react"
 import UserContext from "../contexts/UserContext";
 import styled from "styled-components";
-import { FiHeart } from "react-icons/fi";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { getTimelinePosts, postLike, postUnlike, getLikes} from "../Service";
 
 export default function Likes ({postInfo, setPostsList}){
 
     const {user} = useContext(UserContext);
 
-    //let isLike = (like !== undefined && like === "red") ? "red" : "#ffffff";
-    const [like, setLike] = useState();
 
     function getLike(id){
 
         getLikes(user.token)
-        .then(res => {
-            console.log(res.data)  
-            setLike(res.data.posts)  
+        .then(res => {  
             compareLike(id, res.data.posts)       
         })      
     }
-    console.log(like)
 
     function compareLike(id, likeList){
 
         const likes = likeList.filter((l) => (l.id === id) )
         likes.length !== 0 ? (unLike(id)) : (Like(id))
-        console.log(likes)
     }
 
     function Like(id){
 
-        console.log("entrou no like")
-
         postLike(user.token, id)
-        .then(res => {
-            setLike("deu like")
-            console.log(res.data)
-    
+        .then(res => {   
             getTimelinePosts(user.token)
             .then((res)=> {
             setPostsList(res.data)
@@ -47,39 +36,36 @@ export default function Likes ({postInfo, setPostsList}){
    
     function unLike(id){
 
-        console.log("entrou no unlike")
-
         postUnlike(user.token, id)
-        .then(res => {
-            console.log("deu unlike")
-            console.log(res.data)
-    
+        .then(res => {    
             getTimelinePosts(user.token)
             .then((res)=> {
             setPostsList(res.data)
             })            
         })      
     }
-     
-
-    //    {(habito.done === true) ?
-    //     <Icon onClick={() => Like()} />                   
-    //     : 
-    //     <Icon onClick={() => unLike()} /> 
-    //     } 
-
-
+   
     return(
        <>
-        <Icon onClick={() => getLike(postInfo.id)} />
+       {postInfo.likes.filter((l) => (l.userId === user.id)).length !== 0 ?
+            <IconAiFillHeart style={{color:"#AC0000"}} onClick={() => getLike(postInfo.id)} />
+            :
+            <IconOutlineHeart style={{color:"#ffffff"}} onClick={() => getLike(postInfo.id)} />
+       }
+
         <p>{`${postInfo.likes.length} ${postInfo.likes.length > 1 ? 'likes' : 'like'}`}</p>
        </>
     )
 }
 
-const Icon = styled(FiHeart)`
+const IconAiFillHeart = styled(AiFillHeart)`
     font-size: 20px;
-    color: #ffffff;
+    margin-top: 19px;
+    font-weight: 700;
+`; 
+
+const IconOutlineHeart = styled(AiOutlineHeart)`
+    font-size: 20px;
     margin-top: 19px;
     font-weight: 700;
 `; 
