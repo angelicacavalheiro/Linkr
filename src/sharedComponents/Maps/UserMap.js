1c61d93b3f7ef28f9adae4613f59f4226a8be4b2
Erick Silva <erickrsilva@hotmail.com>
2021-09-23 12:52:28 -0300

    Warning solving and userMap refactor

--- src/sharedComponents/Maps/UserMap.js
+++ src/sharedComponents/Maps/UserMap.js
@@ -1,54 +1,32 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { useRef } from 'react';
-import { useState } from 'react/cjs/react.development';
 import { Marker } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '100%',
-  height: '100%'
-};
 
 
 function UserMap({latitude, longitude}) {
+
+  const containerStyle = {
+    width: '100%',
+    height: '100%'
+  };
+  
+  const center = {
+    lat:Number(latitude), 
+    lng: Number(longitude)
+  }
+  
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
   })
-  
-  const mapRef = useRef(null);
 
-  const [center, setCenter] = useState({lat:0, lng:0})
-
- setTimeout(() => {
-  setCenter({
-    lat: Number(latitude),
-    lng: Number(longitude)
-  });
- }, 500);
-  
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    mapRef.current = map;
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  console.log(map);
-  
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
       >
       <Marker
-        onLoad={onLoad}
         position={center}
       />
 
