ef89e32ac4e761b26937e890a21ae473b901d23e
Erick Silva <erickrsilva@hotmail.com>
2021-09-24 18:15:12 -0300

    Repost confirmation done

--- src/sharedComponents/Repost.js
+++ src/sharedComponents/Repost.js
@@ -3,6 +3,7 @@ import { BiRepost } from "react-icons/bi";
 import { postRepost } from "../Service";
 import React, { useContext, useState } from "react";
 import UserContext from "../contexts/UserContext";
+import ReactModal from "react-modal";
 
 export default function Repost({postInfo}){
 
@@ -10,6 +11,7 @@ export default function Repost({postInfo}){
     const { user } = useContext(UserContext);
     const [colorIntensityCounter, setColorIntensityCounter] = useState(1);
     const [repostCount, setRepostCount] = useState(postInfo.repostCount)
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     let reposted;
 
@@ -23,11 +25,11 @@ export default function Repost({postInfo}){
     const [wasReposted, setWasReposted] = useState(reposted);
     
     function sharePost(){
+        setModalIsOpen(false);
         postRepost(user.token, postId)
             .then(() => {
                 setWasReposted(true);
                 setColorIntensityCounter(colorIntensityCounter + 0.4);
-                
                 setRepostCount(repostCount + 1);
             })
             .catch(() => alert('Erro ao repostar.'));
@@ -35,13 +37,34 @@ export default function Repost({postInfo}){
     
     return(
         <>
-            <RepostIcon wasReposted = {wasReposted} colorIntensity={colorIntensityCounter} onClick={sharePost}/>
-            <RepostCountStyle wasReposted={wasReposted} colorIntensity={colorIntensityCounter} >
-                {repostCount === 0 ?
-                <span>repost</span> : 
-                <span>{repostCount} reposts</span>}
-            </RepostCountStyle>
+            <RepostContainer onClick={() => setModalIsOpen(true)}>
+                <RepostIcon 
+                    wasReposted = {wasReposted} 
+                    colorIntensity={colorIntensityCounter}
+                />
+                <RepostCountStyle wasReposted={wasReposted} colorIntensity={colorIntensityCounter} >
+                    {repostCount === 0 ?
+                    <span>repost</span> : 
+                    <span>{repostCount} reposts</span>}
+                </RepostCountStyle>
+            </RepostContainer>
+            
+            
+            <ModalQueryStyle isOpen={modalIsOpen} style={modalStyle} ariaHideApp={false}>
+                <>
+                    <p>Compartilhar publicação?</p>
+                    <ButtonsContainerStyle>
+                        <button onClick={sharePost}>
+                            Sim
+                        </button>
+                        <button onClick={() => setModalIsOpen(false)}>
+                            Não
+                        </button>
+                    </ButtonsContainerStyle>
+                </>  
+            </ModalQueryStyle>
         </>
+        
     );
 
 }
@@ -59,3 +82,94 @@ const RepostCountStyle = styled.div`
     flex-wrap: wrap;
     padding-bottom: 20px;
 `;
+
+const RepostContainer = styled.div`
+    :hover{
+        cursor: pointer;
+        filter: brightness(0.7);
+    }
+`
+
+const ModalQueryStyle = styled(ReactModal)`
+    height: 250px;
+    width: 55%;
+    top: calc(50vh - 131px);
+    left: calc(50vw - 298px);
+    padding: 30px;
+    position: fixed;
+    background-color: #333333;
+    border: none;
+    border-radius: 50px;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+    outline: none;
+
+    p{
+        font-size: 38px;
+        font-family: 'Oswald', sans-serif;
+        font-weight: 700;
+        text-align: start;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        padding-top: 30px;
+    }
+    
+    @media(max-width: 600px) {
+        height: 200px;
+        width: 100%;
+        top: calc(50vh - 131px);
+        left:0;
+        border-radius: 0;
+        font-size: 20px;
+        padding: 15px 10px;
+        p{font-size: 25px;}
+    }
+`
+
+const modalStyle = {
+    overlay: {
+      backgroundColor: 'rgba(255, 255, 255, 0.5)', 
+     },
+     content: {
+        border: 'none',   
+     }
+ }
+
+ const ButtonsContainerStyle = styled.div`
+    width: 58%;
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 30px;
+
+    button{
+        width: 122px;
+        height: 41px;
+        background-color: #1877F2;
+        border: none;
+        border-radius: 5px;
+        margin-top: 5px;
+
+        font-family: 'Lato', sans-serif;
+        font-weight: 700;
+        color: #FFF;
+
+        :hover{
+            filter: brightness(1.2);
+            cursor: pointer;
+        }
+    }
+
+    @media (max-width:600px){
+        width: 250px;
+
+        button{
+            width: 112px;
+            height: 31px;
+        }
+    }
+ `;
