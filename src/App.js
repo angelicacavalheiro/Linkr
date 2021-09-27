ca6ea7680bcf527079ff6fa5e23642e2062ed242
Erick Silva <erickrsilva@hotmail.com>
2021-09-27 10:44:14 -0300

    Transition group not working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './sharedStyles/reset.css'
-import { BrowserRouter, Switch, Route, Redirect  } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation  } from "react-router-dom";
 import { useState } from 'react';
 import SignUpPage from './components/Signup/SignUpPage'
 import LoginPage from './components/Login/LoginPage'
@@ -12,6 +12,8 @@ import MenuHeaderPage from './components/MenuHeader/MenuHeaderPage';
 import SearchMobile from './components/MenuHeader/SearchMobile';
 import ShowMenuContext from './contexts/ShowMenuContext';
 import UserContext from './contexts/UserContext';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 
 export default function App() {
@@ -19,7 +21,9 @@ export default function App() {
   const storedUser = JSON.parse(localStorage.getItem('storedUser'));
   const [showMenu , setShowMenu] = useState(false)
   const [user, setUser] = useState(storedUser);  
-  const [following, setFollowing]=useState([])
+  const [following, setFollowing]=useState([]);
+  
+  
 
   function disappearMenu() { 
     if(showMenu === true){
@@ -27,56 +31,65 @@ export default function App() {
     }
    }
 
+   const location = useLocation();
     return(     
 
-      <BrowserRouter>
+
         <UserContext.Provider value={{user, setUser}}>
-          <Switch>
+          <TransitionGroup>
+            <CSSTransition
+              timeout={5000}
+              classNames='fade'
+              key={location.key}
+            >
+              <Switch location={location}>
 
-          <ShowMenuContext.Provider value={{disappearMenu, setShowMenu, showMenu, setFollowing, following}}>
+                <ShowMenuContext.Provider value={{disappearMenu, setShowMenu, showMenu, setFollowing, following}}>
          
-              <Route path="/sign-up" exact>
-                <SignUpPage />
-              </ Route> 
+                  <Route path="/sign-up" exact>
+                    <SignUpPage />
+                  </ Route> 
 
-              <Route path="/" exact >              
-                {storedUser ? <Redirect to="/timeline"/> : <LoginPage /> }
-              </Route> 
+                  <Route path="/" exact >              
+                    {storedUser ? <Redirect to="/timeline"/> : <LoginPage /> }
+                  </Route> 
 
-              <Route path="/timeline" exact>
-                <MenuHeaderPage />
-                <SearchMobile />
-                <TimelinePage />
-              </ Route>  
+                  <Route path="/timeline" exact>
+                    <MenuHeaderPage />
+                    <SearchMobile />
+                    <TimelinePage />
+                  </ Route>  
 
-              <Route path="/my-posts" exact>
-                <MenuHeaderPage />    
-                <SearchMobile />  
-                <MyPostsPage />
-              </ Route>
+                  <Route path="/my-posts" exact>
+                    <MenuHeaderPage />    
+                    <SearchMobile />  
+                    <MyPostsPage />
+                  </ Route>
 
-              <Route path="/user/:id" exact>
-                <MenuHeaderPage />
-                <SearchMobile />
-                <UserPage />
-              </ Route>
+                  <Route path="/user/:id" exact>
+                    <MenuHeaderPage />
+                    <SearchMobile />
+                    <UserPage />
+                  </ Route>
 
-              <Route path="/hashtag/:hashtag" exact>
-                <MenuHeaderPage />
-                <SearchMobile />
-                <HashtagPage />
-              </ Route>
+                  <Route path="/hashtag/:hashtag" exact>
+                    <MenuHeaderPage />
+                    <SearchMobile />
+                    <HashtagPage />
+                  </ Route>
 
-              <Route path="/my-likes" exact>
-                <MenuHeaderPage />
-                <SearchMobile />
-                <MyLikesPage />
-              </ Route> 
+                  <Route path="/my-likes" exact>
+                    <MenuHeaderPage />
+                    <SearchMobile />
+                    <MyLikesPage />
+                  </ Route> 
 
-            </ShowMenuContext.Provider>
+                </ShowMenuContext.Provider>
 
-          </ Switch>   
+              </ Switch>
+            </CSSTransition>
+          </TransitionGroup>   
         </UserContext.Provider >    
-      </ BrowserRouter>   
+  
     )
 }
